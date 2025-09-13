61b291051c2e1505700b920b563d3fcdf4e14fa5
Nipun Yadav <nipunyadav6504@gmail.com>
2025-09-13 13:55:55 +0530

    Configured the routing to a certain extent

--- app/product/components/AdditionalProduct.tsx
+++ app/product/components/AdditionalProduct.tsx
@@ -1,39 +1,42 @@
 "use client"
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useReducer } from 'react';
 
 interface ProductCardProps {
-  imageUrl: string;
-  name: string;
+  img_url: string;
+  title: string;
   description: string;
-  availableSizes: number[];
+  id:number
 }
 
 const AdditionalProductCard: React.FC<ProductCardProps> = ({
-  imageUrl,
-  name,
+  img_url,
+  title,
   description,
+  id
 }) => {
+  const router=useRouter()
   return (
     <div className='flex min-w-[350px] h-[25vh] max-h-[25vh] rounded-xl
      bg-white shadow-lg font-sans'>
       <div className='flex-1 h-full overflow-hidden'>
         <img 
-          src={imageUrl} 
-          alt={name} 
+          src={img_url} 
+          alt={title} 
           className='h-[100%] overflow-hidden object-cover rounded-l-xl' 
           
         />
       </div>
       <div className='flex flex-1 flex-col items-center justify-between p-6 gap-y-1'>
         <div className='flex flex-col items-center justify-center gap-2'>
-          <h2 className='text-xl font-bold text-gray-500 -m-2.5 mb-0'>
-            {name.toUpperCase()}
+          <h2 className='text-xl  text-center font-bold text-gray-500 -m-2.5 mb-[-10px] pl-2'>
+            {title}
           </h2>
-          <p className='text-sm text-center leading-relaxed text-gray-400'>
+          <p className='text-sm leading-relaxed text-gray-400'>
             {description}
           </p>
         </div>
-        <button className='py-2 px-3 w-[100%] text-red-500 font-extralight text-sm border-[0.25] 
+        <button onClick={()=>router.push(`/product/${id}`)} className='py-2 px-3 w-[100%] text-red-500 font-extralight text-sm border-[0.25] 
          border-red-500  rounded-md cursor-pointer transition-colors duration-200 hover:bg-amber-500 hover:text-white'>
           Buy Now
         </button>
--- app/product/components/ProductList.tsx
+++ app/product/components/ProductList.tsx
@@ -1,4 +1,10 @@
+"use client"
+import { useEffect, useState } from "react";
 import AdditionalProductCard from "./AdditionalProduct"
+import { Product } from "@/types/Product";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
   const product = {
     imageUrl: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500', // Placeholder image
     name: 'NIKE BLACK',
@@ -7,12 +13,37 @@ import AdditionalProductCard from "./AdditionalProduct"
   };
 
 const ProductList = () => {
+  const [productList, setProductList] = useState<Product[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_NANO_NODE_API_URL}/products/fetch-products`
+        );
+        const data = response.data;
+        setProductList(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, []);
+  
+  if(loading){
+    return (
+          <div className="w-full h-[30vh] shadow-lg flex p-5 overflow-x-scroll gap-5
+    rounded-md bg-white items-center border-amber-400 border-1 justify-center">
+      <Loader2 className="animate-spin text-orange-400 text-2xl" size={40} color={'orange'}/>
+    </div>
+    )
+  }
   return (
     <div className="w-full h-[30vh] shadow-lg flex p-5 overflow-x-scroll gap-5
     rounded-md bg-white items-center border-amber-400 border-1">
-          <AdditionalProductCard {...product}/>
-          <AdditionalProductCard {...product}/>
-          <AdditionalProductCard {...product}/>
+      {productList?productList.map((product)=>(<AdditionalProductCard {...product} key={product.id}/>)):<div></div>}
     </div>
   )
 }
